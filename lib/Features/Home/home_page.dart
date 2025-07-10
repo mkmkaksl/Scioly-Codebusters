a60a1c9530d77ca28dbccc125b1db96ff7edc10c
mkmkaksl <manojkumar@Manojs-MacBook-Pro.local>
2025-07-10 14:20:37 -0500

    Added some bugs in todo.txt

--- lib/Features/Home/home_page.dart
+++ lib/Features/Home/home_page.dart
@@ -9,7 +9,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     GameSetup.init(context);
-    // audioCont?.playBgSound();
+    audioCont?.playBgSound();
 
     return Container(
       decoration: AppTheme.backgroundGradient,
